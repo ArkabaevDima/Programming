7765290b2b60f77e03683d03f81187e428e37c51
ArkabaevDima <43188881+ArkabaevDima@users.noreply.github.com>
2018-10-21 17:00:41 +0300

    getArea of t1

---
+++ Main.java
@@ -0,0 +1,8 @@
+package ru.mirea.arkabaev.lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        Tringle t1 = new Tringle(10.0,15.0,20.0);
+        System.out.println("Area of t1 = " + t1 .getArea());
+    }
+}
