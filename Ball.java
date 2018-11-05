d498b1b978351b1ed28a1d3d6e25c1111eaa38b6
ArkabaevDima <43188881+ArkabaevDima@users.noreply.github.com>
2018-11-05 17:28:26 +0300

    Update

--- Ball.java
+++ Ball.java
@@ -1,22 +1,16 @@
 package ru.mirea.arkabaev.lab1;
 
 public class Ball {
-    public static void main(String[] args) {
-    Balls b1 = new Balls("Red",10);
-    System.out.println(b1);
-    }
-}
-class Balls
-{
-    private String color;
-    private int weight;
 
-    public Balls(String color, int weight)
-    {
-        this.color = color;
-        this.weight = weight;
+        private String color;
+        private int weight;
+
+        public Ball (String color, int weight)
+        {
+            this.color = color;
+            this.weight = weight;
+        }
+        public String toString(){
+            return "Ball is " + color +  " and his weight is " + weight;
+        }
     }
-    public String toString(){
-        return "Ball is "+color+ " and his weight is "+weight;
-    }
-}
